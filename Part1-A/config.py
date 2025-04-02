69852d0fdacfafca3322ec437fb772a534911ee1
Salvatore Gilles Cassarà <gilles2004.sc@gmail.com>
2025-04-02 17:38:07 +0200

    Init

---
+++ Part1-A/config.py
@@ -0,0 +1,15 @@
+# Model Hyperparameters
+lr = 0.0007
+hidden_size = 300
+emb_size = 300
+dropout = 0.1
+clip = 5
+
+# Training settings
+n_epochs = 100
+patience = 10
+
+GPU = "cuda:0"
+CPU = 'cpu'
+
+DEVICE = GPU
