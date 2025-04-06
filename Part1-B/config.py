2dd5a6e505720c79e40e798f6a528ad5737238d6
Salvatore Gilles Cassarà <gilles2004.sc@gmail.com>
2025-04-06 15:57:37 +0200

    Variational Dropout

--- Part1-B/config.py
+++ Part1-B/config.py
@@ -1,12 +1,13 @@
+
 # Model Hyperparameters
-lrs = [0.0001, 0.0007, 0.001, 0.1, 0.3, 0.7, 1, 1.5]
+lr = 1.5
 hidden_size = 300
 emb_size = 300
-dropout = 0.1
-clip = 5
+dropout = 0.3
+clip = 0.25
 
 # Training settings
-n_epochs = 30
+n_epochs = 50
 patience = 3
 
 GPU = "cuda:0"
--- Part1-B/main.py
+++ Part1-B/main.py
@@ -2,10 +2,10 @@ import copy
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from model import LM_RNN
+from model import LM_LSTM
 from functions import init_weights, train_loop, eval_loop
 from utils import lang, train_loader, dev_loader, test_loader
-from config import lrs, emb_size, hidden_size, DEVICE, patience, n_epochs, clip, dropout
+from config import lr, emb_size, hidden_size, DEVICE, patience, n_epochs, clip, dropout
 
 best_ppl = float('inf')
 best_model = None
@@ -13,46 +13,46 @@ best_params = None
 
 vocab_len = len(lang.word2id)
 
-for lr in lrs:
-	# Load the model
-	model = LM_RNN(emb_size, hidden_size, vocab_len, pad_index=lang.word2id["<pad>"]).to(DEVICE)
-	model.apply(init_weights)
+# Load the model
+model = LM_LSTM(emb_size, hidden_size, vocab_len, pad_index=lang.word2id["<pad>"], dropout=dropout).to(DEVICE)
+model.apply(init_weights)
 
-	# Training uses AdamW
-	optimizer = optim.AdamW(model.parameters(), lr=lr)
-	criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
-	criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
+# Training uses SGD
+optimizer = optim.SGD(model.parameters(), lr=lr)
+criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
+criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
 
-	best_local_ppl = float('inf')
-	local_patience = patience
+best_local_ppl = float('inf')
+local_patience = patience
 
-	# Train it
-	for epoch in range(n_epochs):
-		train_loop(train_loader, optimizer, criterion_train, model, clip)
-		ppl_dev, _ = eval_loop(dev_loader, criterion_eval, model)
+# Train it
+for epoch in range(n_epochs):
+#	torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.25)
+	train_loop(train_loader, optimizer, criterion_train, model, clip)
+	ppl_dev, _ = eval_loop(dev_loader, criterion_eval, model)
 
-		print(f"Epoch {epoch+1}: lr={lr}, PPL={ppl_dev}")
+	print(f"Epoch {epoch+1}: dropout={dropout}, PPL={ppl_dev}")
 
-		if ppl_dev < best_local_ppl:
-			best_local_ppl = ppl_dev
-			best_local_model = copy.deepcopy(model)
-			local_patience = patience
-		else:
-			local_patience -= 1
+	if ppl_dev < best_local_ppl:
+		best_local_ppl = ppl_dev
+		best_local_model = copy.deepcopy(model)
+		local_patience = patience
+	else:
+		local_patience -= 1
 
-		if local_patience <= 0:
-			break
+	if local_patience <= 0:
+		break
 
-		if best_local_ppl < best_ppl:
-			best_ppl = best_local_ppl
-			best_model = best_local_model
-			best_params = lr
+	if best_local_ppl < best_ppl:
+		best_ppl = best_local_ppl
+		best_model = best_local_model
+		best_params = dropout
 
 # Evaluate the best model on the test set
 best_model.to(DEVICE)
 final_ppl, _ = eval_loop(test_loader, criterion_eval, best_model)
-print(f'Best PPL: {final_ppl} with lr={best_params}')
+print(f'Best PPL: {final_ppl} with drpout={best_params}')
 
 # Save the best model obtained from training
-path = 'bin/wt_rnn.pt'
+path = 'bin/wt_vd_lstm.pt'
 torch.save(model.state_dict(), path)
